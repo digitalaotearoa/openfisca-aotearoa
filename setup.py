d66e3febf2a94d40b6dcd6f1d408ff75dca3db31
Mauko Quiroga <bot+github@mauko.me>
2022-05-09 14:51:56 +0200

    Upgrade dev dependencies

--- setup.py
+++ setup.py
@@ -1,32 +1,52 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
+"""This file contains OpenFisca-Aotearoa package's metadata and dependencies."""
 
+from pathlib import Path
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()  # pylint: disable=W1514
 
 setup(
-    name='OpenFisca-Aotearoa',
-    version='12.0.0',
-    author='New Zealand Government, Service Innovation Lab',
-    author_email='brenda.wallace@dia.govt.nz,hamish.fraser@dia.govt.nz',
-    description=u'OpenFisca tax and benefit system for Aotearoa',
-    keywords='benefit microsimulation social tax',
-    license='http://www.fsf.org/licensing/licenses/agpl-3.0.html',
-    url='https://github.com/ServiceInnovationLab/openfisca-aotearoa',
+    name="OpenFisca-Aotearoa",
+    version="12.0.0",
+    author="New Zealand Government, Service Innovation Lab",
+    author_email="brenda.wallace@dia.govt.nz,hamish.fraser@dia.govt.nz",
+    description="OpenFisca tax and benefit system for Aotearoa",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="benefit microsimulation social tax",
+    license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
+    url="https://github.com/ServiceInnovationLab/openfisca-aotearoa",
     include_package_data=True,  # Will read MANIFEST.in
+    data_files = [
+        (
+            "share/openfisca/openFisca-aotearoa",
+            ["CHANGELOG.md", "README.md"],
+            ),
+        ],
     install_requires=[
-        'numpy >= 1.21, < 1.22',
-        'openfisca-core[web-api] >= 35, < 36',
+        "numpy >= 1.21, < 1.22",
+        "openfisca-core[web-api] >= 35, < 36",
         ],
     extras_require={
-        'dev': [
-            'flake8 >= 3.4, < 3.7',
-            'flake8-print',
-            'nose',
-            'yamllint'
-            ]
+        "dev": [
+            "autopep8 >= 1.5.4, < 2.0.0",
+            "flake8 >= 3.8.0, < 4.0.0",
+            "flake8-bugbear >= 20.1.0, < 22.0.0",
+            "flake8-builtins >= 1.5.0, < 2.0.0",
+            "flake8-coding >= 1.3.0, < 2.0.0",
+            "flake8-commas >= 2.0.0, < 3.0.0",
+            "flake8-comprehensions >= 3.2.0, < 4.0.0",
+            "flake8-docstrings >= 1.5.0, < 2.0.0",
+            "flake8-import-order >= 0.18.0, < 1.0.0",
+            "flake8-print >= 3.1.0, < 5.0.0",
+            "flake8-quotes >= 3.2.0, < 4.0.0",
+            "flake8-simplify >= 0.9.0, < 1.0.0",
+            "flake8-use-fstring >= 1.1.0, < 2.0.0",
+            "pylint >= 2.6.0, < 3.0.0",
+            "pycodestyle >= 2.6.0, < 3.0.0",
+            ],
         },
     packages=find_packages(),
-    test_suite='nose.collector',
     )
