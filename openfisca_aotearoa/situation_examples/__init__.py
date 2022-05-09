38f5ac47be50ae71d71e4501036d72feda462315
Mauko Quiroga <bot+github@mauko.me>
2022-05-09 17:17:38 +0200

    Ignore todos and fixmes

--- openfisca_aotearoa/situation_examples/__init__.py
+++ openfisca_aotearoa/situation_examples/__init__.py
@@ -8,7 +8,7 @@ DIR_PATH = os.path.dirname(os.path.abspath(__file__))
 
 def parse(file_name):
     file_path = os.path.join(DIR_PATH, file_name)
-    with open(file_path, "r") as file:
+    with open(file_path, "rb") as file:
         return json.loads(file.read())
 
 
