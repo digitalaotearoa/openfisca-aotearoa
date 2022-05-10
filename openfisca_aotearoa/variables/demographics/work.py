1a42dbb916674cfd25a4bd96091b98b40ea93e9c
Mauko Quiroga <bot+github@mauko.me>
2022-05-10 13:46:40 +0200

    Add losing earnings variable

--- openfisca_aotearoa/variables/demographics/work.py
+++ openfisca_aotearoa/variables/demographics/work.py
@@ -1,9 +1,17 @@
-"""TODO: Add missing doctring."""
+"""This module provides work-related demographic variables."""
 
-from openfisca_core.periods import MONTH
+from openfisca_core.periods import DAY, MONTH
 from openfisca_core.variables import Variable
 
 from openfisca_aotearoa.entities import Person
+
+
+class fulltime_employment(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = DAY
+    label = "The person is not in full-time employment"
+    reference = "https://www.legislation.govt.nz/act/public/2018/0032/latest/whole.html#DLM6783146"
 
 
 class hours_per_week_employed(Variable):
@@ -11,3 +19,11 @@ class hours_per_week_employed(Variable):
     entity = Person
     definition_period = MONTH
     label = "The hours per week a person is employed for"
+
+
+class losing_earnings_from_health_injury(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = DAY
+    label = "The person is losing earnings from health injury"
+    reference = "https://www.legislation.govt.nz/act/public/2018/0032/latest/whole.html#DLM6783146"
