e1c771def5945848302e5e6b11f7e32c215100ac
Mauko Quiroga <bot+github@mauko.me>
2022-05-10 14:05:42 +0200

    Extract work gap to variable

--- openfisca_aotearoa/variables/legislation/jobseeker_support.py
+++ openfisca_aotearoa/variables/legislation/jobseeker_support.py
@@ -1,7 +1,5 @@
 """This module provides eligibility and amount for Jobseeker Support."""
 
-import numpy
-
 from openfisca_core import periods, variables
 
 from openfisca_aotearoa import entities
@@ -14,8 +12,5 @@ class jobseeker_support(variables.Variable):
     label = "Jobseeker Support eligibility and amount"
     reference = "https://www.legislation.govt.nz/act/public/2018/0032/latest/whole.html#DLM6783144"
 
-    def formula_2013_07_15(people, period, parameters):
-        fulltime_employment = people("fulltime_employment", period)
-        losing_earnings_from_health_injury = people("losing_earnings_from_health_injury", period)
-
-        return numpy.logical_not(fulltime_employment) + fulltime_employment * losing_earnings_from_health_injury
+    def formula_2013_07_15(people, period):
+        return people("work_gap", period)
