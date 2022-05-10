1353b8a9142f5fac78acb990fff37c39816175cf
Mauko Quiroga <bot+github@mauko.me>
2022-05-10 14:47:48 +0200

    Update formula with calculation

--- openfisca_aotearoa/variables/legislation/jobseeker_support.py
+++ openfisca_aotearoa/variables/legislation/jobseeker_support.py
@@ -12,5 +12,9 @@ class jobseeker_support(variables.Variable):
     label = "Jobseeker Support eligibility and amount"
     reference = "https://www.legislation.govt.nz/act/public/2018/0032/latest/whole.html#DLM6783144"
 
-    def formula_2013_07_15(people, period):
-        return people("work_gap", period)
+    def formula_2013_07_15(people, period, parameters):
+        work_gap = people("work_gap", period)
+        age = people("age", period)
+        net_weekly_benefit = parameters(period).legislation.jobseeker_support.net_weekly_benefit
+
+        return work_gap * net_weekly_benefit[age]
