bf433b19a2c8673e6e547d43e854d1dfc5dbe76b
Mauko Quiroga <bot+github@mauko.me>
2022-05-10 13:23:42 +0200

    Add test prior to benefit commenced

--- openfisca_aotearoa/variables/legislation/jobseeker_support.py
+++ openfisca_aotearoa/variables/legislation/jobseeker_support.py
@@ -15,9 +15,6 @@ class jobseeker_support(variables.Variable):
     reference = "https://www.legislation.govt.nz/act/public/2018/0032/latest/whole.html#DLM6783144"
 
     def formula_2013_07_15(people, period, parameters):
-        # A person is entitled to jobseeker support if the person:
-
-        # Is not in full-time employment.
         fulltime_employment = people("fulltime_employment", period)
 
         return numpy.logical_not(fulltime_employment)
