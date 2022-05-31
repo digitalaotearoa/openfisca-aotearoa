94a8537d14c6ca4693ac45486910b494b9708070
Jonah Duckles <jonah@duckles.org>
2022-05-31 03:02:55 +0000

    loosening numpy version requirements

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
             ),
         ],
     install_requires = [
-        "numpy >= 1.21.0, < 1.22.0",
+        "numpy",
         "openfisca-core[web-api] >= 35.0.0, < 36.0.0",
         ],
     extras_require = {
