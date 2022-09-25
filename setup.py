04b7f24794832c0903c3703b47e78a13a3ec56b2
Hamish Fraser <hamish@verb.co.nz>
2022-09-25 22:00:08 +1300

    Roll version number due to breaking changes

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()  # pylint: disable
 
 setup(
     name = "OpenFisca-Aotearoa",
-    version = "14.0.0",
+    version = "15.0.0",
     author = "New Zealand Government, Service Innovation Lab",
     author_email = "brenda.wallace@dia.govt.nz,hamish.fraser@dia.govt.nz",
     description = "OpenFisca tax and benefit system for Aotearoa",
