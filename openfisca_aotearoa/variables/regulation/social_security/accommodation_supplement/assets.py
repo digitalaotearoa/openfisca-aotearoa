d006bfaade98c4052158eeb0979f786c6588bcc7
Mauko Quiroga <public@mauko.me>
2022-11-24 10:37:25 +0100

    Add 62% to lodging

--- openfisca_aotearoa/variables/regulation/social_security/accommodation_supplement/assets.py
+++ openfisca_aotearoa/variables/regulation/social_security/accommodation_supplement/assets.py
@@ -32,12 +32,14 @@ class accommodation_supplement__assets_requirement(variables.Variable):
 
         total_cash_assets = cash_assets_principal + cash_assets_partners
 
+        # TODO: move to parameter
         ssr2018_15_1_a_i = (
             + people.has_role(entities.Family.PRINCIPAL)
             * (people.family.nb_persons(entities.Family.PARTNER) >= 1)
             * (total_cash_assets <= 16200)
             )
 
+        # TODO: move to parameter
         ssr2018_15_1_a_ii = (
             + people.has_role(entities.Family.PRINCIPAL)
             * (people.family.nb_persons(entities.Family.PARTNER) == 0)
@@ -45,6 +47,7 @@ class accommodation_supplement__assets_requirement(variables.Variable):
             * (total_cash_assets <= 16200)
             )
 
+        # TODO: move to parameter
         ssr2018_15_1_b = (
             + numpy.logical_not(people.has_role(entities.Family.PARTNER))
             * numpy.logical_not(people.has_role(entities.Family.CHILD))
