0922921f0b3b2ba71ca89dbc934a1bdcada1e80e
Mauko Quiroga <public@mauko.me>
2023-07-20 04:51:22 +0200

    Fix rent

--- openfisca_aotearoa/variables/acts/residential_tenancies/interpretation/rent.py
+++ openfisca_aotearoa/variables/acts/residential_tenancies/interpretation/rent.py
@@ -5,6 +5,8 @@ be paid under a tenancy agreement by the tenant.
 
 """
 
+import numpy
+
 from openfisca_core import periods, variables
 
 from openfisca_aotearoa import entities
@@ -23,16 +25,16 @@ class residential_tenancies__rent(variables.Variable):
     definition_period = periods.DateUnit.WEEK
 
     def formula_1987_02_01(tenancy, period, _params):
-        tenant = tenancy("residential_tenancies__tenant", period)
+        tenants = tenancy.members("residential_tenancies__tenant", period)
 
         # rent means any money, goods, services, or other valuable
         # consideration in the nature of rent to be paid or supplied under a
         # tenancy agreement by the tenant;
         housing_costs = tenancy.members("housing_costs", period)
-        rta1986_2_rent_a = housing_costs * tenant
+        rta1986_2_rent_a = tenancy.sum(housing_costs * tenants)
 
         # but does not include any sum of money payable or paid by way of bond
         bond = tenancy("residential_tenancies__bond", period)
-        rta1986_2_rent_b = bond * tenant
+        rta1986_2_rent_b = tenancy.sum(bond * tenants)
 
-        return rta1986_2_rent_a - rta1986_2_rent_b
+        return numpy.maximum(rta1986_2_rent_a - rta1986_2_rent_b, 0)
