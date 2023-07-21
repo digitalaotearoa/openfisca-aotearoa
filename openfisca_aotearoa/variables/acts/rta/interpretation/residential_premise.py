15944e638a51ba5f15430eb4a08a4b23cd24d073
Mauko Quiroga <public@mauko.me>
2023-07-21 17:22:11 +0200

    Add residencial premises with one occupant

---
+++ openfisca_aotearoa/variables/acts/rta/interpretation/residential_premise.py
@@ -0,0 +1,36 @@
+"""Residential tenancies — Residential premise.
+
+Residential premise is a premise used or intended for occupation by any person
+as a place of residence, unlawfully or not.
+
+"""
+
+from openfisca_core import periods, variables
+
+from openfisca_aotearoa import entities
+from openfisca_aotearoa.variables.exegesis.occupation_type import OccupationType
+
+
+class residential_tenancies__residential_premise(variables.Variable):
+    label = "Residential tenancies — Residential premise"
+    reference = "https://www.legislation.govt.nz/act/public/1986/0120/latest/DLM94283.html"
+    documentation = """a premise occupied for residence unlawfully or not."""
+    entity = entities.Premise
+    value_type = bool
+    default_value = False
+    definition_period = periods.DateUnit.WEEK
+
+    def formula_1987_02_01(premises, period, _params):
+        # residential premises means
+
+        # any premises used or intended for occupation by any person
+        occupant = premises.members("occupant", period)
+
+        # as a place of residence
+        occupation_type = premises.members("occupation_type", period)
+        residence = occupation_type == OccupationType.residential
+
+        # whether or not the occupation or intended occupation for residential
+        # purposes is or would be unlawful
+
+        return occupant * residence
